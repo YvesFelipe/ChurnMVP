f230dd638ae87b9ea39354d077728234c1e0f79f
YvesFelipe <144679567+YvesFelipe@users.noreply.github.com>
2023-12-08 12:14:59 -0300

    Delete api - Churn directory

--- api - Churn/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from schemas.cliente_schema import ClienteSchema, ClienteBuscaSchema, ClienteViewSchema, ClienteDelSchema, apresenta_clientes, apresenta_cliente
-                                        
-from schemas.error_schema import ErrorSchema
-                                    
